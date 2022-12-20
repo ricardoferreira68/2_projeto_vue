737480df7834142388dc8b71f6125950ff0e608c
ricardoferreira68 <jrsf68@gmail.com>
2022-12-20 14:42:47 -0300

    Atualização

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e sou {{ profissao }}</p>    
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PrimeiroComponente",
+        data() {
+            return{
+                nome: "Ricardo",
+                profissao: "dev",
+            }
+        }
+    }
+</script>
