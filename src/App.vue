a860b06c8040d3fa9cbb17f42ece6fbf71c3e7a3
ricardoferreira68 <jrsf68@gmail.com>
2022-12-23 09:24:42 -0300

    Atualização

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
-    <!-- <PrimeiroComponente /> -->
-    <!-- <LiveCycle /> -->
-    <pessoa />
+    <Header />
+    <p class="titulo">Hildinair Ciência&Tecnologia</p>
+    <Pessoa />
   </div>
 </template>
 
 <script>
-  // import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import Header from './components/Header.vue'
   import LiveCycle from './components/LiveCycle.vue'
   import Pessoa from "./components/Pessoa.vue";
   export default {
     name: "App",
     components: {
-      // PrimeiroComponente,
+      Header,
       LiveCycle,
       Pessoa,
     }
   }
 </script>
+
+<style>
+  body {
+    background-color: #0f0f0f;
+    color: aliceblue;
+  }
+  a {
+      color:darkgrey;
+  }
+  .menssage {
+    background-color:black;
+  }
+</style>
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,21 +2,26 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades.</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p>Utilizo as seguintes tecnologias de frontend:</p>
         <ul>
-            <li>Python</li>
-            <li>JavaScript</li>
-            <li>Node.js</li>
-            <li>Vue.js</li>
-            <li>Rust</li>
-            <li>Tokio</li>
-            <li>Yew</li>
+            <li v-for="(technology, index) in frontend_technologies" v-bind:key="index">
+                {{ technology }}
+            </li>
         </ul>  
-        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
-        <p>Para acessar o meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <p>Utilizo as seguintes tecnologias de backend:</p>
+        <ul>
+            <li v-for="technology in backend_technologies" v-bind:key="technology.id">
+                {{ technology.technology }}
+            </li>
+        </ul>  
         <div>
-            <button></button>
+            <button @click="showEmail">
+                <span v-if="mostrar_email">Acultar e-mail</span>
+                <span v-else>Mostar e-mail</span>
+            </button>
         </div>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p class="menssage">Para acessar o meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
         <Picture />
     </div>
 </template>
@@ -35,7 +40,26 @@ export default {
             mostrar_email: false,
             email: "jrsf68@gmail.com",
             meu_link: "https://google.com",
+            frontend_technologies: ['HTML', 'CSS', 'JavaScript', 'Vue.js', 'Yew'],
+            backend_technologies: [
+                {id: 1, technology: 'Python'}, 
+                {id: 2, technology: 'JavaScript'}, 
+                {id: 3, technology: 'Rust'}, 
+                {id: 4, technology: 'PHP'},
+            ],
         };
+    },
+    methods: {
+        showEmail() {
+            this.mostrar_email = !this.mostrar_email;
+        }
     },
 }
 </script>
+
+<style>
+    .titulo {
+        font-size: 40px;
+        font-weight: bold;
+    }
+</style>
